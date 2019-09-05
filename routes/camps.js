fb54237c75076430179dcc6001ea9bb11db6b072
glamboyosa <ogbemudiatimothy@gmail.com>
2019-09-06 00:58:54 +0100

    added export to camps camps route

--- routes/camps.js
+++ routes/camps.js
@@ -1,16 +1,16 @@
-const { Camp, Validate } = require("../models/camp");
-const express = require("express");
+const { Camp, Validate } = require('../models/camp');
+const express = require('express');
 const router = express.Router();
-const auth = require("../middleware/auth");
-const admin = require("../middleware/admin");
-router.get("/", async (req, res) => {
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
+router.get('/', async (req, res) => {
   const camp = await Camp.find();
   res.send(camp);
 });
 
 //MOST LIKELY WILL REPLACE THIS POST METHOD WITH A METHOD THAT SEEDS THE MONGO DATABASE. JUST CURRENTLY IMPLEMENTING POST
 //BECAUSE OF TESTING REASONS
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = Validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   const camp = new Camp({
@@ -21,7 +21,7 @@ router.post("/", async (req, res) => {
   await camp.save();
   res.send(camp);
 });
-router.put("/:id", [auth, admin], async (req, res) => {
+router.put('/:id', [auth, admin], async (req, res) => {
   const { error } = Validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   const camp = await Camp.findByIdAndUpdate(req.params.id, {
@@ -31,3 +31,4 @@ router.put("/:id", [auth, admin], async (req, res) => {
   });
 });
 //I WONT IMPLEMENT A DELETE METHOD, JUST DELETE FROM MONGODB
+module.exports = router;
