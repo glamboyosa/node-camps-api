44a0bb0c71f505751386c20492973c69b8d63628
glamboyosa <ogbemudiatimothy@gmail.com>
2019-09-06 01:30:25 +0100

    added auth and admin middleware to the camps route handler

--- routes/camps.js
+++ routes/camps.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 
 //MOST LIKELY WILL REPLACE THIS POST METHOD WITH A METHOD THAT SEEDS THE MONGO DATABASE. JUST CURRENTLY IMPLEMENTING POST
 //BECAUSE OF TESTING REASONS
-router.post('/', async (req, res) => {
+router.post('/', [auth, admin], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   const camp = new Camp({
